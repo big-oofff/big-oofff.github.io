b3a3b63d42b2fc5c835c5f2ea3f18a50cecf1a8b
Thomas Xiao <78974883+big-oofff@users.noreply.github.com>
2026-08-22 21:18:55 -0700

    Credit original portfolio template author

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,7 +9,7 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Built by {greeting.title} with React.
+          Built by {greeting.title} with React. Original template by{" "}\n          <a\n            href="https://github.com/ashutosh1919"\n            target="_blank"\n            rel="noopener noreferrer"\n          >\n            ashutosh1919\n          </a>\n          .
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
